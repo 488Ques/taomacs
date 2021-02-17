71908b1efc82134b4a7a613bd7bdb0cdb0d96796
Tri <abeces968@gmail.com>
2021-02-18 01:54:00 +0700

    Initial commit

---
+++ init.el
@@ -0,0 +1,42 @@
+;; Setting up packagaes
+(require 'package)
+(setq package-enable-at-startup nil)
+(add-to-list 'package-archives
+	     '("melpa" . "https://melpa.org/packages/"))
+(package-initialize)
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(unless (package-installed-p 'dracula-theme)
+  (package-refresh-contents)
+  (package-install 'dracula-theme))
+
+ (when (version<= "9.2" (org-version))
+   (require 'org-tempo))
+
+;; Configuration
+(org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
+
+;; Auto generated
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default default default italic underline success warning error])
+ '(ansi-color-names-vector
+   ["#2e3436" "#a40000" "#4e9a06" "#c4a000" "#204a87" "#5c3566" "#729fcf" "#eeeeec"])
+ '(custom-enabled-themes '(dracula))
+ '(custom-safe-themes
+   '("2dff5f0b44a9e6c8644b2159414af72261e38686072e063aa66ee98a2faecf0e" default))
+ '(package-selected-packages
+   '(dired-sidebar delight flycheck company go-mode eglot powerline dashboard rainbow-delimiters hungry-delete sudo-edit ace-window rainbow-mode rainbow avy -t smex ido-vertical-mode which-key dracula-theme use-package)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :extend nil :stipple nil :background "#282a36" :foreground "#f8f8f2" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 138 :width normal :foundry "ADBO" :family "Source Code Pro")))))
