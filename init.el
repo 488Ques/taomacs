75538fbc87cb84dd22ab6ee1ca977ac07d83dfa3
Tri <abeces968@gmail.com>
2021-03-13 00:37:34 +0700

    Commit to test git credential storage.

--- init.el
+++ init.el
@@ -39,4 +39,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :extend nil :stipple nil :background "#282a36" :foreground "#f8f8f2" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 138 :width normal :foundry "ADBO" :family "Source Code Pro")))))
+ )
