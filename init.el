f02074ef46a846cb68685171493bb96217bfaa27
Tri <abeces968@gmail.com>
2021-03-13 04:16:59 +0700

    Switch from ansi-term/bash to using eshell.

--- init.el
+++ init.el
@@ -31,7 +31,7 @@
    ["#2e3436" "#a40000" "#4e9a06" "#c4a000" "#204a87" "#5c3566" "#729fcf" "#eeeeec"])
  '(custom-enabled-themes '(dracula))
  '(custom-safe-themes
-   '("2dff5f0b44a9e6c8644b2159414af72261e38686072e063aa66ee98a2faecf0e" default))
+   '("81c3de64d684e23455236abde277cda4b66509ef2c28f66e059aa925b8b12534" "2dff5f0b44a9e6c8644b2159414af72261e38686072e063aa66ee98a2faecf0e" default))
  '(package-selected-packages
    '(exec-path-from-shell go-mode swiper diminish smart-hungry-delete magit dired-sidebar flycheck company eglot powerline dashboard rainbow-delimiters sudo-edit ace-window rainbow-mode rainbow avy -t smex ido-vertical-mode which-key dracula-theme use-package)))
 (custom-set-faces
@@ -39,4 +39,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(default ((t (:inherit nil :extend nil :stipple nil :background "#282a36" :foreground "#f8f8f2" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 130 :width normal :foundry "PfEd" :family "Source Code Pro")))))
