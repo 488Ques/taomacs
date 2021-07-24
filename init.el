badc46cad6b7671e7f9176efd44d65c1b0567424
Trirst <abeces968@gmail.com>
2021-07-25 01:29:01 +0700

    Fix rainbow-mode and rainbow-delimiters.

--- init.el
+++ init.el
@@ -31,12 +31,21 @@
    ["#2e3436" "#a40000" "#4e9a06" "#c4a000" "#204a87" "#5c3566" "#729fcf" "#eeeeec"])
  '(custom-enabled-themes '(dracula))
  '(custom-safe-themes
-   '("81c3de64d684e23455236abde277cda4b66509ef2c28f66e059aa925b8b12534" "2dff5f0b44a9e6c8644b2159414af72261e38686072e063aa66ee98a2faecf0e" default))
+   '("171d1ae90e46978eb9c342be6658d937a83aaa45997b1d7af7657546cae5985b" "549ccbd11c125a4e671a1e8d3609063a91228e918ffb269e57bd2cd2c0a6f1c6" "81c3de64d684e23455236abde277cda4b66509ef2c28f66e059aa925b8b12534" "2dff5f0b44a9e6c8644b2159414af72261e38686072e063aa66ee98a2faecf0e" default))
+ '(fci-rule-color "#3E4451")
  '(package-selected-packages
-   '(exec-path-from-shell go-mode swiper diminish smart-hungry-delete magit dired-sidebar flycheck company eglot powerline dashboard rainbow-delimiters sudo-edit ace-window rainbow-mode rainbow avy -t smex ido-vertical-mode which-key dracula-theme use-package)))
+   '(exec-path-from-shell go-mode swiper diminish smart-hungry-delete magit dired-sidebar flycheck company eglot powerline dashboard rainbow-delimiters sudo-edit ace-window rainbow-mode rainbow avy -t smex ido-vertical-mode which-key dracula-theme use-package))
+ '(tetris-x-colors
+   [[229 192 123]
+    [97 175 239]
+    [209 154 102]
+    [224 108 117]
+    [152 195 121]
+    [198 120 221]
+    [86 182 194]]))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :extend nil :stipple nil :background "#282a36" :foreground "#f8f8f2" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 130 :width normal :foundry "PfEd" :family "Source Code Pro")))))
+ '(default ((t (:inherit nil :extend nil :stipple nil :background "#282a36" :foreground "#f8f8f2" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 150 :width normal :foundry "ADBO" :family "Hack")))))
