ca080439760e7d7f516fb3caa3633c8c2e6c8ef3
Trirst <abeces968@gmail.com>
2021-07-25 22:33:34 +0700

    Initialize emmet-mode and Vuejs' language server

--- init.el
+++ init.el
@@ -37,7 +37,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(web-mode dracula which-key use-package swiper sudo-edit smex smart-hungry-delete rainbow-mode rainbow-delimiters powerline page-break-lines magit ido-vertical-mode go-mode flycheck exec-path-from-shell eglot dracula-theme dired-sidebar diminish dashboard company ace-window)))
+   '(emmet-mode web-mode dracula which-key use-package swiper sudo-edit smex smart-hungry-delete rainbow-mode rainbow-delimiters powerline page-break-lines magit ido-vertical-mode go-mode flycheck exec-path-from-shell eglot dracula-theme dired-sidebar diminish dashboard company ace-window)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
