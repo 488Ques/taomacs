3d34307255bdad92f3c9cc414ee07b9ccb34c3a9
tri.nguyen <ngminhtri002@gmail.com>
2024-02-23 01:06:44 +0700

    Add side padding and set fringe background color to white in org-mode buffer

--- layers/org_mode.el
+++ layers/org_mode.el
@@ -1,6 +1,16 @@
-;; Outline-based notes management and organizer
-;; (global-set-key (kbd "C-c l") #'org-store-link)
-(global-set-key (kbd "C-c a") #'org-agenda)
+(use-package org
+  :bind
+  ("C-c a" . org-agenda)
+  :hook
+  (org-mode . visual-line-mode)
+  (org-mode . (lambda ()
+		(custom-set-faces '(fringe ((t (:background "white")))))
+		(setq left-fringe-width 30
+		      right-fringe-width 30))))
+
+(use-package org-modern
+  :hook
+  ((org-mode . org-modern-mode)))
 
 ;; Additional Org-mode related functionality
 (use-package org-contrib)
