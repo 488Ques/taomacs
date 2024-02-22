4e2bdc6d4c36a42d7f3dbecd1699074c36d0e519
tri.nguyen <ngminhtri002@gmail.com>
2024-02-23 01:39:38 +0700

    Remove org-modern. Enable org-indent-mode.

--- layers/org_mode.el
+++ layers/org_mode.el
@@ -1,16 +1,21 @@
 (use-package org
+  :init
+  (setq org-indent-indentation-per-level 1)
   :bind
   ("C-c a" . org-agenda)
   :hook
   (org-mode . visual-line-mode)
-  (org-mode . (lambda ()
-		(custom-set-faces '(fringe ((t (:background "white")))))
-		(setq left-fringe-width 30
-		      right-fringe-width 30))))
+  (org-mode . org-indent-mode)
+  ;; (org-mode . (lambda ()
+  ;;		(custom-set-faces '(fringe ((t (:background "white")))))
+  ;;		(setq left-fringe-width 30
+  ;;		      right-fringe-width 30)))
+  )
 
-(use-package org-modern
-  :hook
-  ((org-mode . org-modern-mode)))
+;; Total style conversion for org-mode
+;; (use-package org-modern
+;;   :hook
+;;   ((org-mode . org-modern-mode)))
 
 ;; Additional Org-mode related functionality
 (use-package org-contrib)
